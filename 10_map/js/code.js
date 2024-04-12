e0044427f256527d7e36941cdbe15e8ac2f29898
AlexCesde <145169173+AlexCesde@users.noreply.github.com>
2024-04-12 17:02:45 -0500

    Update code.js
    New functions

--- 10_map/js/code.js
+++ 10_map/js/code.js
@@ -7,7 +7,24 @@ function example() {
     colors.set('pastel', 'pink');
 
     out += `<h1>Map: ${colors.get('Metallic')}</h1>`;
-    
+    out += `<h1>Size: ${colors.size}</h1>`;
+
+    // Iteration
+    for (item of colors) {
+        out += `<h1>Items: ${item}</h1>`;
+    }
+
+    for (item of colors.keys()) {
+        out += `<h1>Keys: ${item}</h1>`;
+    }
+
+    colors.forEach((value, key) => {
+        out += `<h1>Values: ${value} - ${key}</h1>`;
+    });
+
+    const colorsIterators = colors.values();
+    out += `<h1>Iterator: ${colorsIterators.next().value}</h1>`;
+
     document.getElementById("root").innerHTML = out;
 }
 
